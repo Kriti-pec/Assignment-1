0284770d6e96be9202a317d21f428dd51dca9bfc
Kriti-pec <113089013+Kriti-pec@users.noreply.github.com>
2022-09-15 15:35:27 +0530

    Update Ques1.java

--- Assignment 1/Ques1.java
+++ Assignment 1/Ques1.java
@@ -1,10 +1,15 @@
-class Ques1{
+import java.util.Scanner;
+class Ques1 {
     public static void main(String[] args) {
-       int num1 = 5;
-       int num2 = 78;
-       int addition_of_num1_and_num2 = num1+num2;
-       System.out.println("Addition of num1 and num2: "+addition_of_num1_and_num2);
-       int subtraction_of_num1_and_num2 = num2-num1;
-       System.out.println("Subtraction of num1 from num2: "+subtraction_of_num1_and_num2);
+        Scanner s = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int num1 = s.nextInt();
+        System.out.print("Input second number: ");
+        int num2 = s.nextInt();  
+        s.close();
+        int addition_of_num1_and_num2 = num1 + num2;
+        System.out.println("Addition of num1 and num2: " + addition_of_num1_and_num2);
+        int subtraction_of_num1_and_num2 = num2 - num1;
+        System.out.println("Subtraction of num1 from num2: " + subtraction_of_num1_and_num2);
     }
 }
